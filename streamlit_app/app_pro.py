0004e30e62137f7df6ca66456cb666b50c5e0a98
yves046 <yvestoure717@gmail.com>
2025-12-13 17:01:40 +0000

    Update app_pro.py

--- streamlit_app/app_pro.py
+++ streamlit_app/app_pro.py
@@ -27,7 +27,7 @@ if "paid" not in st.session_state:
         else:
             with st.spinner("Redirection sécurisée vers Stripe..."):
                 try:
-                    r = requests.post( "https://vida-secure-backend-2025.onrender.com/create-checkout-session",
+                    r = requests.post( "https://vida-secure-ai.vercel.app/create-checkout-session",,
                         json={"email": email.strip()},
                         timeout=15
                     )
