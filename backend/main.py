0aa8fa34ff612e8f889976b0cca4f9521adc344a
touré yves <toureyves@MacBook-Pro-15-YVES-TOURE.local>
2026-05-24 23:13:37 +0000

    fix imports

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
 from intrusion import start_detection
-from backend import models
+import models
 import threading
 import os
 import requests
